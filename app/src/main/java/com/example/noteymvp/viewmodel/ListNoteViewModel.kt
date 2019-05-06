8727ba9985d89f63aab711364ae1c45296be263d
angitha <angitha@qburst.com>
2019-05-06 16:26:19 +0530

    Repository

--- app/src/main/java/com/example/noteymvp/viewmodel/ListNoteViewModel.kt
+++ app/src/main/java/com/example/noteymvp/viewmodel/ListNoteViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.noteymvp.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.example.noteymvp.database.Note
+import com.example.noteymvp.repository.NoteRepository
 
-class ListNoteViewModel : ViewModel() {
+class ListNoteViewModel(private val noteRepository: NoteRepository) : ViewModel() {
+
+        fun getNotes(context: Context) = noteRepository.getNotes(context)
+
+        fun addNote(title: String, details: String, context: Context) = noteRepository.addNote(title, details, context)
+
+        fun updateNote(note: Note, context: Context) = noteRepository.updateNote(note, context)
+
+        fun deleteNote(id: String, context: Context) = noteRepository.deleteNote(id, context)
 
 }
